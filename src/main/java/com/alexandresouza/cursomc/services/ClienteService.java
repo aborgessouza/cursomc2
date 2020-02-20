48d94c43999c7e3786a546e2d611df35a8b2ef35
Alexandre Borges Souza <aborgessouza@gmail.com>
2020-02-20 19:52:34 -0300

    Ajustando e testando a deleção de Cliente

--- src/main/java/com/alexandresouza/cursomc/services/ClienteService.java
+++ src/main/java/com/alexandresouza/cursomc/services/ClienteService.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.alexandresouza.cursomc.domain.Cidade;
 import com.alexandresouza.cursomc.domain.Cliente;
@@ -41,6 +42,7 @@ public class ClienteService {
 
 	}
 	
+	@Transactional
 	public Cliente insert (Cliente obj) {
 		obj.setId(null);
 		obj = repo.save(obj);
@@ -61,7 +63,7 @@ public class ClienteService {
 			repo.deleteById(id);
 		}
 		catch (DataIntegrityViolationException e ) {
-			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas.");
+			throw new DataIntegrityException("Não é possível excluir porque há pedidos registrados.");
 		}
 		
 	}
